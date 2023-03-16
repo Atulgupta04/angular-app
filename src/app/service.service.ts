91f86358bbe08074c1b3a4707e1d90004e996a1a
Atul Gupta <atulcullen09@gmail.com>
2023-03-16 13:02:52 +0530

    angular-connect-nodejs

---
+++ src/app/service.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Todo } from './model/todo';
+import { BehaviorSubject } from 'rxjs';
+import { Router} from '@angular/router'
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  dataArray:any[]=[]
+  data:any={
+  Id:0,
+  name:'',
+  email:''
+  }
+name:any
+
+  constructor(private http: HttpClient,private router:Router) {
+    const localData=localStorage.getItem('studentdetail');
+    console.log(localData);
+  }
+
+
+isLoggedIn=new BehaviorSubject<boolean>(false)
+
+url="http://localhost:3000/register"
+url1="http://localhost:3000/login"
+
+
+
+// users(){
+//   return this.http.get<[]>(this.url)
+// }
+
+sendData(data:any){
+  this.isLoggedIn.next(true);
+  return this.http.post(this.url1,data)
+
+}
+
+saveUser(data){
+  this.isLoggedIn.next(true);
+  return this.http.post(this.url,data)
+
+}
+
+
+
+addData(){
+  this.data.Id=this.dataArray.length+1
+  this.dataArray.push(({
+    Id:this.data.Id,
+    name:this.data.name,
+    email:this.data.email}
+    ));
+  console.log(this.data,this.dataArray)
+  localStorage.setItem('studentdetail',JSON.stringify(this.dataArray))
+  this.data={
+    Id:0,
+    name:'',
+    email:''
+    }
+}
+
+editData(item:any){
+  this.data=item
+  localStorage.setItem('studentdetail',JSON.stringify(this.data))
+
+}
+
+updateData(){
+  const record=this.dataArray.find(m=>m.Id==this.data.Id)
+  localStorage.setItem('studentdetail',JSON.stringify(record))
+  record.name=this.name;
+}
+
+removeData(id:any){
+for(let i=0; i<this.dataArray.length; i++){
+  if(this.dataArray[i].Id ==id){
+    this.dataArray.splice(i,1);
+  }
+}
+
+
+}}
+
+
